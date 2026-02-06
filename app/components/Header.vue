35c2b3d802c3af91165a85d52d4a0ec04e582236
zep69 <nikita0609991@gmail.com>
2026-02-06 17:45:22 +0400

    gsap add

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -2,7 +2,12 @@
     <div class="header">
         <div class="header--btn-row">
             <div>
-                <button class="primary-button">Click</button>
+                <h2>Twitch pidarasi</h2>
+            </div>
+            <div class="btn-wrapper">
+                <button class="btn-group btn-group--start">Главная</button>
+                <button class="btn-group">О проекте</button>
+                <button class="btn-group btn-group--end">Войти</button>
             </div>
         </div>
     </div>
@@ -15,12 +20,9 @@
 <style scoped lang="scss">
 .header{
     height: 70px;
-    border: thin solid var(--secondary-color);
-    background-color: var(--bg-color-secondary);
+    border: none;
+    background-color: none;
     border-radius: 15px;
-    -webkit-box-shadow: 4px 4px 15px 0px var(--primary-color);
-    -moz-box-shadow: 4px 4px 15px 0px var(--primary-color);
-    box-shadow: 4px 4px 15px 0px var(--primary-color);
     &--btn-row  {
         display: flex;
         justify-content:space-between;
@@ -29,5 +31,29 @@
         padding: 0px 10px 0 10px;
     }
             
+}
+.btn-wrapper {
+    -webkit-box-shadow: 4px 4px 15px 0px var(--primary-color);
+    -moz-box-shadow: 4px 4px 15px 0px var(--primary-color);
+    box-shadow: 4px 4px 15px 0px var(--primary-color);
+}
+.btn-group{
+    background-color: none;
+    color: var(--primary-color);
+    background: none;
+    font-size: 15px;
+    padding: 8px;
+    border: solid var(--primary-color) thin;
+    &--start{
+        border-radius: 5px 0 0 5px;
+    }
+    &--end{
+        border-radius: 0 5px 5px 0;
+    }
+}
+.btn-group:hover{
+    background-color: var(--primary-color);
+    color: var(--bg-color-secondary);
+    cursor: pointer;
 }
 </style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,83 @@
 <template>
-    <div>
-        <h1>Абичный пейдж</h1>
+    <div style="margin-top: 20px;padding-left: 20px;padding-right: 20px;">
+        <Header />
+        <div class="body-main">
+            <div style="display:  flex; justify-content: start;">
+                <h2 class="box-h">Добро пожаловать на сайт!</h2>
+                <div class="box-s" style="width: 400px;margin-left: 10px;">
+                    <span>Сайтик для интеграции с Twitch, даст вам возможность взаимодействовать с трансляцией</span>
+                </div>
+            </div>
+        </div>
+        <section>
+            <div class="fade-up" style="color: var(--text-color-secondary);font-size: 50px;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+            <div class="fade-up" style="color: var(--text-color-secondary);font-size: 50px;">
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in tincidunt enim. Sed sit amet libero iaculis, aliquet metus eu, tempus tellus. Etiam nec urna purus. Cras facilisis tristique lorem sagittis tincidunt. Nulla tempor purus at dui consectetur, in pellentesque lectus vestibulum. Morbi vitae imperdiet erat. Etiam nec vulputate nulla, nec interdum nisl.
+
+In molestie erat ut turpis consequat, in ultricies nulla faucibus. Cras nec commodo justo. Suspendisse nisl dui, accumsan sit amet egestas at, consectetur sit amet lacus. Praesent id purus magna. Nam id magna gravida nulla condimentum semper a ut metus. Donec cursus, turpis ullamcorper posuere ornare, nunc lacus tristique nulla, nec pharetra arcu velit id ipsum. Nullam finibus mauris nec purus porttitor viverra vitae nec tortor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum facilisis et felis non aliquet. Pellentesque aliquam arcu ac leo euismod vestibulum. Donec vestibulum pulvinar sapien sit amet sodales. Sed non consequat dolor, at rhoncus odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi dictum odio tortor, at facilisis mi commodo vitae. Mauris tristique velit dolor, ut scelerisque ligula elementum non.
+
+Duis eu facilisis massa. Vestibulum finibus ex id ullamcorper fringilla. Etiam malesuada egestas augue, in condimentum quam elementum ac. Sed pulvinar ipsum eros, sit amet venenatis erat eleifend et. Praesent id leo sit amet sem finibus finibus. Pellentesque turpis urna, consequat eget ligula vel, porta porttitor sem. Vestibulum malesuada venenatis eros, nec mollis velit cursus eget. Curabitur pretium nibh eu libero malesuada, quis pretium magna faucibus. Donec lacinia consectetur risus ut pellentesque. Cras dictum, massa ac maximus fringilla, tortor libero hendrerit ex, ac sodales enim velit et ante. Sed odio odio, varius et bibendum ac, hendrerit at metus. In eleifend tempus justo, id efficitur nunc tincidunt sed. Aenean sed volutpat ligula. Nulla rhoncus enim a ultricies mattis. Ut dignissim feugiat scelerisque. Donec a sodales tortor. </div>
+            <div class="fade-up" style="color: var(--text-color-secondary);font-size: 50px;">freestar
+freestar
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in tincidunt enim. Sed sit amet libero iaculis, aliquet metus eu, tempus tellus. Etiam nec urna purus. Cras facilisis tristique lorem sagittis tincidunt. Nulla tempor purus at dui consectetur, in pellentesque lectus vestibulum. Morbi vitae imperdiet erat. Etiam nec vulputate nulla, nec interdum nisl.
+
+In molestie erat ut turpis consequat, in ultricies nulla faucibus. Cras nec commodo justo. Suspendisse nisl dui, accumsan sit amet egestas at, consectetur sit amet lacus. Praesent id purus magna. Nam id magna gravida nulla condimentum semper a ut metus. Donec cursus, turpis ullamcorper posuere ornare, nunc lacus tristique nulla, nec pharetra arcu velit id ipsum. Nullam finibus mauris nec purus porttitor viverra vitae nec tortor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum facilisis et felis non aliquet. Pellentesque aliquam arcu ac leo euismod vestibulum. Donec vestibulum pulvinar sapien sit amet sodales. Sed non consequat dolor, at rhoncus odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi dictum odio tortor, at facilisis mi commodo vitae. Mauris tristique velit dolor, ut scelerisque ligula elementum non.
+
+Duis eu facilisis massa. Vestibulum finibus ex id ullamcorper fringilla. Etiam malesuada egestas augue, in condimentum quam elementum ac. Sed pulvinar ipsum eros, sit amet venenatis erat eleifend et. Praesent id leo sit amet sem finibus finibus. Pellentesque turpis urna, consequat eget ligula vel, porta porttitor sem. Vestibulum malesuada venenatis eros, nec mollis velit cursus eget. Curabitur pretium nibh eu libero malesuada, quis pretium magna faucibus. Donec lacinia consectetur risus ut pellentesque. Cras dictum, massa ac maximus fringilla, tortor libero hendrerit ex, ac sodales enim velit et ante. Sed odio odio, varius et bibendum ac, hendrerit at metus. In eleifend tempus justo, id efficitur nunc tincidunt sed. Aenean sed volutpat ligula. Nulla rhoncus enim a ultricies mattis. Ut dignissim feugiat scelerisque. Donec a sodales tortor.
+
+Ut accumsan lorem eget blandit elementum. Aliquam dictum nulla eu mollis venenatis. Vivamus a sem eros. Ut rutrum magna nec erat tristique, nec consectetur tellus luctus. Duis sit amet lectus in elit venenatis viverra eget vel augue. Sed eget felis massa. Nullam vehicula, justo a lacinia tristique, leo augue fermentum diam, id mollis sapien mauris et ante.
+
+Suspendisse posuere augue id lobortis commodo. Phasellus ultricies, lorem ut viverra consectetur, enim tellus malesuada enim, sit amet tincidunt enim dui vel libero. Quisque vitae pellentesque velit, a scelerisque metus. Donec viverra velit non arcu efficitur pulvinar. Nulla ac diam et tellus lobortis gravida sed sit amet nibh. Nam dui felis, convallis sed efficitur ac, ornare et nunc. Pellentesque ac interdum massa, quis commodo purus. Quisque a scelerisque ante, et bibendum mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus fringilla tortor rhoncus odio tincidunt, ac eleifend augue dictum. Nullam tempus eget est non ultricies. Pellentesque erat sem, egestas in libero eget, congue tristique orci. Nullam maximus congue pretium. </div>
+        </section>
     </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue'
 
+const { $gsap } = useNuxtApp()
+
+onMounted(() => {
+  $gsap.utils.toArray<HTMLElement>('.fade-up').forEach(el => {
+    $gsap.from(el, {
+      y: 40,
+      opacity: 0,
+      duration: 0.6,
+      ease: 'power2.out',
+      scrollTrigger: {
+        trigger: el,
+        start: 'top 85%',
+        toggleActions: 'play none none reverse'
+      }
+    })
+  })
+  const tl = $gsap.timeline()
+  tl.from('.box-h', {
+    x: -100,
+    opacity: 0,
+    duration: 1,
+    ease: 'power2.out'
+  }).from('.box-s', {
+    x: 100,
+    opacity: 0,
+    duration: 1,
+    ease: 'power2.out'
+  }, '-=0.4')
+})
 </script>
 
 <style scoped>
-
+.body-main{
+    padding: 10px 20px 10px 20px;
+}
+.body-main h2{
+    color: var(--primary-color);
+    font-size: 30px;
+}
+span{
+    color: var(--text-color-secondary);
+    font-size: 18px;
+}
 </style>
